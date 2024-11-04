c27820a9ba67b9007ba5df9b033d3a1e96bda050
Munna Shaik <munafshaik4711@gmail.com>
2024-11-04 19:26:22 +0530

    new changes in AMI

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-02801556a781a4499"  # Replace with a valid AMI ID for your region
+  ami           = "ami-06b21ccaeff8cd686"  # Replace with a valid AMI ID for your region
   instance_type = "t2.micro"  # Change instance type if needed
 
   tags = {
